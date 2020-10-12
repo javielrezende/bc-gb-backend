502ec7d58b415c75b548e87cbfa3410aafa05cea
Roger Rezende <javielrezende@gmail.com>
2020-10-12 11:19:12 -0300

    :wrench: Lint, prettier and editor conf working
    Lint, prettier and editor conf working

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { json } from 'express';
+import routes from './routes';
 
 const port = 3333;
 
@@ -6,13 +7,13 @@ const app = express();
 app.use(express.json());
 
 app.post('/user', (request, response) => {
-    const {name, email} = request.body;
+  const { name, email } = request.body;
 
-    const user = {name, email};
+  const user = { name, email };
 
-    return response.json(user)
-})
+  return response.json(user);
+});
 
 app.listen(port, () => {
-    console.log(`🚀 Server started on port ${port}`);
-})
+  console.log(`🚀 Server started on port ${port}`);
+});
