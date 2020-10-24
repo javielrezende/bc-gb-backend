af945c0b0d046fbd5696f0c9992c97ab13edfc53
Roger Rezende <javielrezende@gmail.com>
2020-10-24 18:27:05 -0300

    :sparkles: Manage user with encrypted password
    Manage user with encrypted password

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -15,7 +15,7 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parseDate = parseISO(date);
 
@@ -23,7 +23,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parseDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
