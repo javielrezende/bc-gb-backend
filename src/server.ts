605c24d744bcc706b93cf596029c598653aece93
Roger Rezende <javielrezende@gmail.com>
2020-11-08 20:53:32 -0300

    :sparkles: Add route to get assets
    Add route to get assets

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import uploadConfig from './config/upload';
 
 const PORT = 3333;
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(PORT, () => {
