172ee5c5416aef77dfb2387826271a46f77fa78d
Roger Rezende <javielrezende@gmail.com>
2020-11-08 21:35:14 -0300

    :sparkles: Add global error handler
    Add global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const PORT = 3333;
 
@@ -11,6 +13,24 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server started on port ${PORT}`);
