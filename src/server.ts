20ea01c17e9f970ea72dd501bba7f3d9f179e87c
Roger Rezende <javielrezende@gmail.com>
2020-11-18 16:51:01 -0300

    :sparkles: Add cors
    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import './database';
@@ -10,6 +11,8 @@ import AppError from './errors/AppError';
 const PORT = 3333;
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
